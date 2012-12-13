be6e2c2c648500b42c627be35663307dabba1437
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2012-12-13 15:20:35 -0500

    Added dev folder in config for kev to get when he clones

---
+++ config/development/database.php
@@ -0,0 +1,8 @@
+<?php 	
+
+define('DB_TYPE','mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'meet');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('MODEL_DIR', 'application/models/');
---
+++ config/development/paths.php
@@ -0,0 +1,6 @@
+<?php
+
+// Always provide a trailing slash (/) after a path
+define('URL' , '/growple/meet/v1.0/');
+define('LIBS', 'libs/');
+define('ENVIRONMENT', 'development');
