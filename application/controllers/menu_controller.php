31bd703de730f7d57f97e0b63b034b392afae98a
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2012-12-14 01:39:17 -0500

    Filled database with dummy data for now. Finished index method for controller, prepares items and submodules for view

--- application/controllers/menu_controller.php
+++ application/controllers/menu_controller.php
@@ -10,6 +10,32 @@ class Menu extends Controller {
 	// throws error for proper logging and error notification - FHM
 	public function index(){
 
+		$module_list = Module::find_all_by_parent_id('1');
+		$menues = array();
+
+		// go through list of all items & submodules for menu module - FHM
+		foreach ($module_list as $key => $submodule) {
+			
+			$menues[$key] = $submodule->to_array();
+
+			$item_list = Item::find_all_by_module_id($submodule->id);
+
+			foreach ($item_list as $item) {
+				$menues[$key]['items'][] = $item->to_array(); 
+			}
+		}
+
+		// check if first time in menu - FHM
+		$first_time = Session::get('menu_first_time');
+		$first_time = isset($first_time) ? 'No' : 'Yes'; 
+	
+		$info = array('menues' => $menues, 'is_virgin' => $first_time);
+		parent::$data = $info; 
+		
 		parent::index();
+	}
+
+	public function cartCheckout(){
+
 	}
 }
--- application/views/common/header.php
+++ application/views/common/header.php
@@ -3,11 +3,8 @@
 
 <head>
 	<title>Currently updating...</title>
-
 	<!-- STYLESHEET-->
 	<link rel="stylesheet" type="text/css" href="<?php echo URL . 'public/css/main.css'; ?>">
-	<!-- JAVASCRIPTS -->
-	<script type="text/javascript" src="javascripts/jquery-1.7.1.min.js"></script>
 </head>	
 <body>
 
