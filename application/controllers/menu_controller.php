2908c92ecab5947aa30b312a0bd8df84ccdd7ed3
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2012-12-14 01:47:46 -0500

    Added exception handlers to index method

--- application/controllers/menu_controller.php
+++ application/controllers/menu_controller.php
@@ -10,29 +10,43 @@ class Menu extends Controller {
 	// throws error for proper logging and error notification - FHM
 	public function index(){
 
-		$module_list = Module::find_all_by_parent_id('1');
-		$menues = array();
-
-		// go through list of all items & submodules for menu module - FHM
-		foreach ($module_list as $key => $submodule) {
-			
-			$menues[$key] = $submodule->to_array();
-
-			$item_list = Item::find_all_by_module_id($submodule->id);
-
-			foreach ($item_list as $item) {
-				$menues[$key]['items'][] = $item->to_array(); 
-			}
+		try{
+			$module_list = Module::find_all_by_parent_id('1');
+		}catch(Exception $e){
+			$this->handleError('danger', get_class().'_controller.php', 'Problem pulling menu info data database, ORM/DB problem.');
 		}
 
-		// check if first time in menu - FHM
-		$first_time = Session::get('menu_first_time');
-		$first_time = isset($first_time) ? 'No' : 'Yes'; 
-	
-		$info = array('menues' => $menues, 'is_virgin' => $first_time);
-		parent::$data = $info; 
+		$menues = array();
+
+		if(!empty($module_list)){
+			// go through list of all items & submodules for menu module - FHM
+			foreach ($module_list as $key => $submodule) {
+				
+				$menues[$key] = $submodule->to_array();
+
+				try{
+					$item_list = Item::find_all_by_module_id($submodule->id);	
+				}catch(Exception $e){
+					$this->handleError('danger', get_class().'_controller.php', 'Problem pulling item data from database, ORM/DB problem.');
+				}
+				
+				foreach ($item_list as $item) {
+					$menues[$key]['items'][] = $item->to_array(); 
+				}
+			}
+
+			// check if first time in menu - FHM
+			$first_time = Session::get('menu_first_time');
+			$first_time = isset($first_time) ? 'No' : 'Yes'; 
 		
-		parent::index();
+			$info = array('menues' => $menues, 'is_virgin' => $first_time);
+			parent::$data = $info; 
+
+			parent::index();
+
+		}else{
+			$this->handleError('danger', get_class().'_controller.php', 'Problem displaying menues and items.');
+		}
 	}
 
 	public function cartCheckout(){
