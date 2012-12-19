c7a77e6bfa7221dc7b982be4239e0040edc8b6f7
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2012-12-19 00:14:41 -0500

    Took out burns fn, slowing down system way too much, not worth it

--- application/views/home.php
+++ application/views/home.php
@@ -1,33 +1,26 @@
-<script>
-	$(document).ready(function() {
-		home();
-	});
-</script>
 <!-- START COLUMN1 - INTERACTIVE MENU -->
 <div class="shadow" id="shadowMenu"></div>
 <div id="menu_intmenu" class="homeSection"></div>	
 <div class="sectionLinks" id="btn_intmenu">
-		<a href="#"><img src="<?php echo URL . 'public/img/home/btn_intmenu.png'; ?>"></a>	
+		<a href="<?php echo URL . 'menu'; ?>" ><img src="<?php echo URL . 'public/img/home/btn_intmenu.png'; ?>"></a>	
 		<!-- SLIDESHOW FOR INTERACTIVE MENU MENU -->	
 		<div id="slideshow_intmenu"></div>	
 </div>	
 <!-- END INTERACTIVE MENU -->
-
 <!-- START COLUMN2 - GAMES-->
 <div class="shadow" id="shadowGames"></div>
 <div id="menu_games" class="homeSection"></div>	
 <div class="sectionLinks" id="btn_games">
-		<a href="#"><img src="<?php echo URL . 'public/img/home/btn_game.png'; ?>"></a>	
+		<a href="<?php echo URL . 'home'; ?>"><img src="<?php echo URL . 'public/img/home/btn_game.png'; ?>"></a>	
 		<!-- SLIDESHOW FOR GAMES MENU -->	
 		<div id="slideshow_games"></div>	
 	</div>	
 <!-- END GAMES -->
-
 <!-- START COLUMN3 - VIDEOS -->
-<div class="shadow" id="shadowVideos"></div>
+<div class="shadow"  id="shadowVideos"></div>
 <div id="menu_videos" class="homeSection"></div>
 <div class="sectionLinks" id="btn_videos">
-		<a href="#"><img src="<?php echo URL . 'public/img/home/btn_video.png'; ?>"></a>	
+		<a href="<?php echo URL . 'videos'; ?>" ><img src="<?php echo URL . 'public/img/home/btn_video.png'; ?>"></a>	
 	<!-- SLIDESHOW FOR VIDEOS MENU -->	
 	<div id="slideshow_videos"></div>	
 </div>		
