79aa8c12cb87852df35d947193909018b94ff055
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2013-01-08 21:08:10 -0500

    Added pressed to all buttons, made it show/hide give that real button feel

--- public/js/main.js
+++ public/js/main.js
@@ -74,7 +74,6 @@ function error(){
 // list of functions according to main pages - FHM
 function home(){
 
-
 	var appCache = window.applicationCache;
 	
 	// on checking for both refresh and loading	
@@ -105,26 +104,33 @@ function home(){
 	$(document).ready(function() {	
 		$("#menuLink").click(function(){
 			$(this).attr("src", URL  + "public/img/home/btn_intmenu_pressed.png");
-				// push and unpush - FHM	
-				setTimeout(function() { 
-					setTimeout(function(){$("body").load(URL + "menu");},300); 
-			}, 100);
+			 setTimeout(function(){ 
+			 	$("#menuLink").attr("src", URL  + "public/img/home/btn_intmenu.png");
+			 	 setTimeout(function(){ 
+					$("body").load(URL + "menu");
+			 	 });
+			 }, 300);
 		});
+
 
 		$("#gameLink").click(function(){
 			$(this).attr("src", URL  + "public/img/home/btn_game_pressed.png");
-				// push and unpush - FHM	
-				setTimeout(function() { 
-					setTimeout(function(){$("body").load(URL + "game");},300); 
-			}, 100);
+			 setTimeout(function(){ 
+			 	$("#gameLink").attr("src", URL  + "public/img/home/btn_game.png");
+			 	 setTimeout(function(){ 
+					$("body").load(URL + "game");
+			 	 });
+			 }, 300);
 		});
 
 		$("#videoLink").click(function(){
 			$(this).attr("src", URL  + "public/img/home/btn_video_pressed.png");
-				// push and unpush - FHM	
-				setTimeout(function() { 
-					setTimeout(function(){$("body").load(URL + "video");},300); 
-			}, 100);
+			 setTimeout(function(){ 
+			 	$("#videoLink").attr("src", URL  + "public/img/home/btn_video.png");
+			 	 setTimeout(function(){ 
+					$("body").load(URL + "video");
+			 	 });
+			 }, 300);
 		});
 
 		$("#menu_intmenu").click(function(){
@@ -182,9 +188,15 @@ function home(){
 function menu(menus, user_basket){
 
 	$("#menuHome").click(function(){
-		$.post(URL + 'menu/saveBasket/d/', {user_basket: basket});
-		$("body").load(URL + "home");
-		$.post(URL + 'home/stepOut');
+		$(this).attr("src", URL  + "public/img/common/btn_home_pressed.png");
+		 setTimeout(function(){ 
+		 	$("#menuHome").attr("src", URL  + "public/img/common/btn_home.png");
+		 	 setTimeout(function(){ 
+		 	 	$.post(URL + 'menu/saveBasket/d/', {user_basket: basket});
+				$("body").load(URL + "home");
+				$.post(URL + 'home/stepOut');
+		 	 });
+		 }, 300);
 	});
 
 	// counter for grabIt function - FHM
@@ -236,8 +248,6 @@ function menu(menus, user_basket){
 			}).fadeTo(200, 1);
 		});
 	});
-
-	
 
 	function changePicture(pic_path){
 
@@ -510,11 +520,19 @@ function video(videos){
 	showRandomVideo();	
 
 	$("#videoHomeLink").click(function(){
-		$("body").load(URL + "home");
+		$("#videoHomeLink").attr("src", URL  + "public/img/common/btn_home_pressed.png");
+		 setTimeout(function(){ 
+		 	$("#videoHomeLink").attr("src", URL  + "public/img/common/btn_home.png");
+		 	 setTimeout(function(){ $("body").load(URL + "home");}, 500);
+		 }, 300);
 	});
 
 	$("#next").click(function(event) {
-		showRandomVideo();	
+		$(this).attr("src", URL  + "public/img/video/btn_next_pressed.png");
+		 setTimeout(function(){ 
+		 	$("#next").attr("src", URL  + "public/img/video/btn_next.png");
+		 	 setTimeout(function(){ showRandomVideo();}, 500);
+		 }, 300);
 	});
 
 	// check menu and video status, stop or play accordingly - FHM
