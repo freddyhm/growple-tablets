a19672f884b1517c2f755155759b4a883c70c58e
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2013-01-29 12:42:51 -0500

    working on index, trying to figure out how to separate the models

--- index.php
+++ index.php
@@ -54,13 +54,6 @@ $login = new Login();
 
 
 
-$cfg = ActiveRecord\Config::instance();
-
-$cfg->set_default_connection("admin");
-$cfg->set_model_directory('application/models/admin');
-
-print_r(User::find("all"));
-
 print_r($cfg);
 break;
 $app = new Bootstrap($login);
