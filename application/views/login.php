e2a8961be67980451d737846b3c5b77fb717b933
dh9kim <kevin.kim@growple.com>
2013-02-03 22:54:06 -0500

    fixed login.php

--- application/views/login.php
+++ application/views/login.php
@@ -32,9 +32,12 @@
       </script>           
         <form method="post" action="index.php" name="loginform" id="loginform">
             <div class="login">
+                <div id="login_form">
+                    Welcome to Growple Meet, Please Sign In<br><br>
                 <input id="login_input_username" class="login_input" type="text" name="user_name" placeholder="Username" />
                 <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" placeholder="Password"/>                    
-                <input type="submit"  name="login" value="Submit" />                 
+                <input type="submit"  name="login" value="Login" />      
+                </div>           
             </div>    
         </form>
         <div>
