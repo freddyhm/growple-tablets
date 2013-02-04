6a9d5df0ae2fa818a56a03fbbb5853ea4026efb3
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2013-02-04 13:28:03 -0500

    updated bidloo

--- config/staging/owl/Database.php
+++ config/staging/owl/Database.php
@@ -5,6 +5,6 @@
     ActiveRecord\Config::initialize(function($cfg)
     {
         $cfg->set_model_directory('application/models/');
-        $cfg->set_connections(array('owl' => 'mysql://bidloo_admin:rAMwbSmdnNbng2cViv8ocMXPvJTa87@localhost/bidloo_meet_owl;charset=utf8'));
+        $cfg->set_connections(array('owl' => 'mysql://bidlootk_admin:rAMwbSmdnNbng2cViv8ocMXPvJTa87@localhost/bidlootk_meet_owl;charset=utf8'));
         $cfg->set_default_connection("owl");
     });
