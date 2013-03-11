bd16b2a14bca910cca30a3850fd9f28314e704f1
dh9kim <kevin.kim@growple.com>
2013-03-11 19:28:46 -0400

    Finished the first draft of the HTML and CSS of Discover

--- application/views/discover.php
+++ application/views/discover.php
@@ -3,31 +3,34 @@
 		<table class="promo-list">
 			<tr>
 					<td>
-					<div class="promo-item">
+					<div id="thumb1" class="promo-item">
 						<span id="promo-title-promo">FREE APPETIZER</span>
 						<span class="promo-desc">Locked</span>
+						<div class="promo-lock-overlay"></div>
+						<div class="promo-thumb-overlay"></div>
 						<div class="promo-name-overlay"></div>
-						<div class="promo-item-thumb">
-							<img src="../img/discover/item1.png">
-						</div>		
+				
 					</div>
 					</td>
 					<td>
-					<div class="promo-item">
-						<span id="promo-title-event">UFC FIGHTNIGHT</span>
+					<div id="thumb2" class="promo-item">
+						<span id="promo-title-event">UFC FIGHT NIGHT</span>
 						<span class="promo-desc">Mar.14th</span>
+						<div class="promo-thumb-overlay"></div>
 						<div class="promo-name-overlay"></div>	
 					</div>
 					</td>
 					<td>
-					<div class="promo-item">
+					<div id="thumb3" class="promo-item">
 						<span id="promo-title-special">WING SPECIAL</span>
 						<span class="promo-desc">Mondays</span>
+						<div class="promo-thumb-overlay"></div>
 						<div class="promo-name-overlay"></div>	
 					</div>
 					</td>
 					<td>
 					<div class="promo-item">
+						<div class="promo-thumb-overlay"></div>
 						<div class="promo-name-overlay"></div>	
 					</div>
 					</td>
@@ -37,11 +40,34 @@
 		
 	<div id="discover-featured">
 		<div class="discover-divider"></div>
+		<img class="discover-icon" src="<?php echo URL . 'public/img/discover/icon_featured.png'; ?>">
 		<span class="discover-title">FEATURED ITEMS</span>	
 			<table class="feature-list">
 			<tr>
-					<td><div class="feature-item"></div></td>
-					<td><div class="feature-item"></div></td>
+					<td>
+						<div id="feat-thumb1" class="feature-item">
+							<div class="feature-title">Bulgogi (불고기)</div>
+							<div class="feature-price">$9.99</div>
+							<div class="feature-name-overlay"></div>
+
+						</div>
+					</td>
+					<td>
+						<div id="feat-thumb1" class="feature-item">
+							<div class="feature-title">Bulgogi (불고기)</div>
+							<div class="feature-price">$9.99</div>
+							<div class="feature-name-overlay"></div>
+
+						</div>
+					</td>
+					<td>
+					<div id="feat-thumb1" class="feature-item">
+							<div class="feature-title">Bulgogi (불고기)</div>
+							<div class="feature-price">$9.99</div>
+							<div class="feature-name-overlay"></div>
+
+						</div>
+					</td>
 					<td><div class="feature-item"></div></td>
 					<td><div class="feature-item"></div></td>
 			</tr>
@@ -49,16 +75,4 @@
 
 	</div>	
 
-	<div id="discover-chef">
-		<div class="discover-divider"></div>
-		<span class="discover-title">CHEF'S PICK</span>	
-		<table class="feature-list">
-			<tr>
-					<td><div class="feature-item"></div></td>
-					<td><div class="feature-item"></div></td>
-					<td><div class="feature-item"></div></td>
-					<td><div class="feature-item"></div></td>
-			</tr>
-		</table> 
-	</div>	
 </div>	
