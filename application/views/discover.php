1d42b493cbb3c899f9763dbb237b71f198bd819b
dh9kim <kevin.kim@growple.com>
2013-03-10 22:42:11 -0400

    working on discover

--- application/views/discover.php
+++ application/views/discover.php
@@ -2,11 +2,63 @@
 	<div id="discover-promo">
 		<table class="promo-list">
 			<tr>
-					<td></td>
-					<td></td>
-					
+					<td>
+					<div class="promo-item">
+						<span id="promo-title-promo">FREE APPETIZER</span>
+						<span class="promo-desc">Locked</span>
+						<div class="promo-name-overlay"></div>
+						<div class="promo-item-thumb">
+							<img src="../img/discover/item1.png">
+						</div>		
+					</div>
+					</td>
+					<td>
+					<div class="promo-item">
+						<span id="promo-title-event">UFC FIGHTNIGHT</span>
+						<span class="promo-desc">Mar.14th</span>
+						<div class="promo-name-overlay"></div>	
+					</div>
+					</td>
+					<td>
+					<div class="promo-item">
+						<span id="promo-title-special">WING SPECIAL</span>
+						<span class="promo-desc">Mondays</span>
+						<div class="promo-name-overlay"></div>	
+					</div>
+					</td>
+					<td>
+					<div class="promo-item">
+						<div class="promo-name-overlay"></div>	
+					</div>
+					</td>
 			</tr>
 		</table> 
-		<div id="discover-divider"></div>
+	</div>	
+		
+	<div id="discover-featured">
+		<div class="discover-divider"></div>
+		<span class="discover-title">FEATURED ITEMS</span>	
+			<table class="feature-list">
+			<tr>
+					<td><div class="feature-item"></div></td>
+					<td><div class="feature-item"></div></td>
+					<td><div class="feature-item"></div></td>
+					<td><div class="feature-item"></div></td>
+			</tr>
+		</table> 
+
+	</div>	
+
+	<div id="discover-chef">
+		<div class="discover-divider"></div>
+		<span class="discover-title">CHEF'S PICK</span>	
+		<table class="feature-list">
+			<tr>
+					<td><div class="feature-item"></div></td>
+					<td><div class="feature-item"></div></td>
+					<td><div class="feature-item"></div></td>
+					<td><div class="feature-item"></div></td>
+			</tr>
+		</table> 
 	</div>	
 </div>	
