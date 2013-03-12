fa4822a29e2468caf07da08527a1fafd425463bf
dh9kim <kevin.kim@growple.com>
2013-03-11 20:07:27 -0400

    finished play.php

---
+++ application/views/play.php
@@ -0,0 +1,15 @@
+<div id="play-area">
+	<video id="play-video" width="1024" height="600" autoplay="true" src=""></video>
+</div>
+<!-- START VIDEO MENUS -->
+<div id="play-menu">
+	<div id="play-title">
+		<span id="play-name">HANGOVER 3</span>
+		<br>
+		<span id="play-author">MOVIE TRAILER</span>
+	</div>	
+	<div id="btn-play-next">
+		<img id="play-next" src="<?php echo URL . 'public/img/play/btn-next.png'; ?>">
+	</div>
+</div>
+
