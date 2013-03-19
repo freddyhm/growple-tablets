40dbb3d416c14f3ea416e58b9bfbf561924ca700
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2013-03-19 09:25:34 -0400

    updated prod db config

--- config/production/owl/Database.php
+++ config/production/owl/Database.php
@@ -4,7 +4,7 @@
 
     ActiveRecord\Config::initialize(function($cfg)
     {
-        $cfg->set_model_directory(MODEL_DIR);
-        $cfg->set_connections(array('owl' => 'mysql://meekoca_admin:rAMwbSmdnNbng2cViv8ocMXPvJTa87@localhost/meekoca_meet_owl;charset=utf8'));
+        $cfg->set_model_directory('application/models/');
+        $cfg->set_connections(array('owl' => 'mysql://meekoca_admin:9.-xp2$jwPvV<rD[jaBWR9Ms6_c`KW@localhost/meekoca_meet_owl;charset=utf8'));
         $cfg->set_default_connection("owl");
     });
