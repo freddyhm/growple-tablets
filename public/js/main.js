e91e05de20134abae560bd218426acee6ba2389c
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2013-04-05 17:29:47 -0400

    added condition adding unlove if submod is hot

--- public/js/main.js
+++ public/js/main.js
@@ -506,7 +506,15 @@ function discover(hotItems, infoItems, specialsItems, commentList){
 
                 var itemSelector = "#item" + val.id;
 
-                var item = "<div class='item' id='item" + val.id + "'><div class='image'><img class='one' src='" + URL + "public/img/discover/" + submod + "/" + val.big_pic + "'><div class='bg'></div></div><div class='text'><div class='bg'></div><div class='title'><span>" + val.name.toUpperCase() + "</span><br><span class='prices'>" + val.price + "</span></div><div class='desc'><span>" + val.description + "</span></div><div class='heart unlove'><img src='" + URL + "public/img/discover/btn-unheart-unpressed.png'></div><div class='heart love'><img src='" + URL + "public/img/discover/btn-heart-unpressed.png'><div class='count'></div></div></div><div class='unlove-selection'><div class='unlove-selection-pop'><div class='thanks-unlove'><p>Thanks for the feeback!</p></div>" + comments + "</div></div></div>";
+                var unlove = "<div class='heart unlove'><img src='" + URL + "public/img/discover/btn-unheart-unpressed.png'></div><div class='unlove-selection'><div class='unlove-selection-pop'><div class='thanks-unlove'><p>Thanks for the feeback!</p></div>" + comments + "</div></div></div>";
+
+                var love = "<div class='heart love'><img src='" + URL + "public/img/discover/btn-heart-unpressed.png'><div class='count'></div></div></div>";
+
+                var item = "<div class='item' id='item" + val.id + "'><div class='image'><img class='one' src='" + URL + "public/img/discover/" + submod + "/" + val.big_pic + "'><div class='bg'></div></div><div class='text'><div class='bg'></div><div class='title'><span>" + val.name.toUpperCase() + "</span><br><span class='prices'>" + val.price + "</span></div><div class='desc'><span>" + val.description + "</span></div>" + love;
+
+                if(submod == 'hot'){
+                    item += unlove;
+                }
 
                 $(".slider").append(item);
 
